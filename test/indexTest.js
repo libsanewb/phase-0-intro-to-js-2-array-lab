7e43212452f8420fa693fbfda978214475b84428
estifanos <libsanewb@gmail.com>
2023-03-12 23:57:31 -0400

    assignment is done

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,29 @@
+
+
+ //const cats=cats();
+  const cats=['Milo', 'Otis', 'Garfield'];
+
+//   function Array(){
+//   //cats.push('Milo', 'Otis', 'Garfield');
+//   return cats;
+// }
+  //cats.length = 0;
+
+//case 2
+function destructivelyAppendCat(name) {
+
+  return cats.push(name);
+}
+//case 3
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
+}
+
+
 require ( './helpers.js' );
 
 describe('index.js', function () {
+  
   describe('cats', function () {
     it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
       expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
@@ -8,16 +31,15 @@ describe('index.js', function () {
   });
 
   describe('Array functions', function () {
-    beforeEach(function () {
+   const cats=  beforeEach(function () {
       cats.length = 0;
-
+        
       cats.push('Milo', 'Otis', 'Garfield');
     });
 
     describe('destructivelyAppendCat(name)', function () {
       it('appends a cat to the end of the cats array', function () {
         destructivelyAppendCat('Ralph');
-
         expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield", "Ralph"]);
       });
     });
@@ -25,7 +47,6 @@ describe('index.js', function () {
     describe('destructivelyPrependCat(name)', function () {
       it('prepends a cat to the beginning of the cats array', function () {
         destructivelyPrependCat("Bob");
-
         expect(cats).to.have.ordered.members(["Bob", "Milo", "Otis", "Garfield"]);
       });
     });
